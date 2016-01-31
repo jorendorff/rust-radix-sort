41bf83e0e1a1b86ca6793a43051c2b49290aed10
Jason Orendorff <jason.orendorff@gmail.com>
2016-01-31 14:54:57 -0600

    benchmarks, yay

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+#[cfg(test)] extern crate test;
+
 pub fn radix_sort(data: &mut [i32]) {
     let mut buckets = vec![vec![]; 256];
 
@@ -23,9 +27,50 @@ pub fn radix_sort(data: &mut [i32]) {
 
 }
 
-#[test]
-fn it_works() {
-    let mut data = vec![512, 4, 3, -1, 256, 1, 6];
-    radix_sort(&mut data);
-    assert_eq!(data, vec![-1, 1, 3, 4, 6, 256, 512]);
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+    use super::radix_sort;
+
+    #[test]
+    fn it_works() {
+        let mut data = vec![512, 4, 3, -1, 256, 1, 6];
+        radix_sort(&mut data);
+        assert_eq!(data, vec![-1, 1, 3, 4, 6, 256, 512]);
+    }
+
+    // This function returns the same vector of random-looking values each time
+    // it's called.
+    fn generate_unsorted() -> Vec<i32> {
+        let mut v = vec![0; 512 * 1024];
+
+        // Fill v with the same numbers each time this benchmark is run.
+        let mut x: u64 = 17;
+        for i in 0 .. v.len() {
+            v[i] = x.wrapping_mul(2685821657736338717) as i32;
+            x ^= x >> 12;
+            x ^= x << 25;
+            x ^= x >> 27;
+        }
+
+        v
+    }
+
+    #[bench]
+    fn radix_sort_big_data(b: &mut Bencher) {
+        let v = generate_unsorted();
+        b.iter(|| {
+            let mut my_v = v.clone();
+            radix_sort(&mut my_v);
+        });
+    }
+
+    #[bench]
+    fn std_sort_big_data(b: &mut Bencher) {
+        let v = generate_unsorted();
+        b.iter(|| {
+            let mut my_v = v.clone();
+            my_v.sort();
+        });
+    }
 }
